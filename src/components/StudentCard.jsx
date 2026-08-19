3200184e9416096196c694cc1c6d846b1abdc498
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-08-19 23:33:19 +0530

    assignment 1

---
+++ src/components/StudentCard.jsx
@@ -0,0 +1,14 @@
+
+const StudentCard = ({stu}) => {
+  return (
+    <div>
+        {stu.map(()=>{
+            
+        })
+
+}
+    </div>
+  )
+}
+
+export default StudentCard
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -5,14 +5,6 @@ const TaskCard = ({task}) => {
 <div>
   <h3 className="text-5xl">{task.title}</h3>
   <p>{task.category}</p>
-       <div className="flex">
-  <button className="p-3 bg-green-500">
-        Completed
-      </button>
-      <button className="p-3 bg-red-500">
-        Pending
-      </button>
-  </div>
 </div> 
  );
 }
